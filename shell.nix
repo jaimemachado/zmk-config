77eaa1e32b1a9bf2f38c831e479d7fa0287f645d
Jaime Machado Neto <jaime.machado@gmail.com>
2025-10-11 09:50:37 +0200

    Add Taskfile and update shell.nix for zmk-viewer installation and build tasks

--- shell.nix
+++ shell.nix
@@ -1,12 +1,45 @@
-with import <nixpkgs> { };
-mkShell {
-  NIX_LD_LIBRARY_PATH = lib.makeLibraryPath [
-    gcc
-  ];
-  NIX_LD = lib.fileContents "${stdenv.cc}/nix-support/dynamic-linker";
+let
+  pkgs = import <nixpkgs> {};
+  gnuArm = if pkgs ? gcc-arm-embedded then pkgs.gcc-arm-embedded else pkgs.arm-none-eabi-gcc;
+  pythonEnv = pkgs.python3.withPackages (ps: [
+    ps.west
+    ps.pip
+    ps.pyelftools
+    ps.pyyaml
+    ps.packaging
+  ]);
+in pkgs.mkShell {
+  ZEPHYR_TOOLCHAIN_VARIANT = "gnuarmemb";
+  GNUARMEMB_TOOLCHAIN_PATH = toString gnuArm; # Path for Zephyr to locate toolchain
 
-  # nativeBuildInputs is usually what you want -- tools you need to run
-  nativeBuildInputs = with pkgs.buildPackages; [
+  buildInputs = with pkgs; [
+    pythonEnv
+    cmake
+    ninja
+    gperf
+    dtc
+    git
+    gcc
+    diffutils
+    file
+    unzip
+    which
+    gnuArm
+    dfu-util
+    openocd
     go
   ];
+
+  shellHook = ''
+    echo "[ZMK] Toolchain variant: $ZEPHYR_TOOLCHAIN_VARIANT -> $GNUARMEMB_TOOLCHAIN_PATH"
+    if ! command -v west >/dev/null; then
+      echo "[WARN] west not found in PATH";
+    fi
+  # Ensure Go bin (for zmk-viewer) is on PATH if it exists
+  export PATH="$HOME/go/bin:$PATH"
+    if [ ! -d .west ]; then
+      echo "[ZMK] Run: west init -l config && west update";
+    fi
+    echo "[ZMK] Example: west build -b nice_nano_v2 -- -DSHIELD=sofle_left"
+  '';
 }
